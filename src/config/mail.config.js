589070c0749b6afed74933da0c62970eebbc4c3d
Abhishek Kumar <meaviishek@gmail.com>
2025-12-14 11:04:32 +0530

    change

--- src/config/mail.config.js
+++ src/config/mail.config.js
@@ -3,7 +3,8 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true only for 465
+  secure:process.env.SMTP_PORT == 465,
+
   auth: {
     user: process.env.SMTP_ADMINISTRATOR,
     pass: process.env.SMTP_ADMINISTRATOR_PASS,
